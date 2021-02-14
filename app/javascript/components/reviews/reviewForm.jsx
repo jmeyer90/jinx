b65fa819b7567455d2c8ebcf4e9713d7aeffdb4f
Justin Meyer <meyerj.justin@yahoo.com>
2021-02-13 20:31:23 -0500

    fix create review form

--- app/javascript/components/reviews/reviewForm.jsx
+++ app/javascript/components/reviews/reviewForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import { createReview } from '../../actions/review_actions';
@@ -7,18 +7,31 @@ import useInput from '../../utils/form_input'
 const POSSIBLE_RATINGS = [1,2,3,4,5];
 
 const ReviewForm = () => {
-  const [body, setBody] = useInput("")
-  const [rating, setRating] = useInput(1)
+  const [body, setBody] = useState("")
+  const [rating, setRating] = useState(1)
+  const dispatch = useDispatch()
+  const { businessId } = useParams()
+
+  const handleSubmit = (e, rating, body) => {
+    e.preventDefault
+  
+    dispatch(createReview(businessId, rating, body))
+  }
 
   return (
     <form onSubmit={e => handleSubmit(e, rating, body) }>
-      { selectRating(setRating) }
+      {POSSIBLE_RATINGS.map((rating)=> (
+        <label key={rating}>
+          <input type='radio' name='rating' value={rating}
+          onChange={ e => setRating(e.target.value) } required/>
+      </label>
+      ))}
 
       <input type="text" 
         name="review[body]" 
         placeholder="Write a review" 
         value={body} 
-        onChange={ setBody }/>
+        onChange={ e => setBody(e.target.value) }/>
 
       <button>Submit Review</button>
     </form>
@@ -31,19 +44,12 @@ const selectRating = (setRating) => {
       {POSSIBLE_RATINGS.map((rating)=> (
         <label key={rating}>
           <input type='radio' name='rating' value={rating}
-          onChange={ setRating } required/>
+          onChange={ e => setRating(e.target.value) } required/>
       </label>
       ))}
     </section>
   )
 }
 
-const handleSubmit = (e, rating, body) => {
-  e.preventDefault
-  const dispatch = useDispatch()
-  const { businessId } = useParams()
-
-  dispatch(createReview(businessId, rating, body))
-}
 
 export default ReviewForm
