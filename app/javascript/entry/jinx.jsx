43d1c0470d795dea3bbb7e6e92dfdc557daa1f0f
Justin Meyer <meyerj.justin@yahoo.com>
2021-02-15 21:54:23 -0500

    add current_user to window

--- app/javascript/entry/jinx.jsx
+++ app/javascript/entry/jinx.jsx
@@ -4,7 +4,29 @@ import Root from './root';
 import configureStore from '../store/store'
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  let store = configureStore()
+  let preloadedState;
+
+  if (window.current_user) {
+    preloadedState = {
+      entities: {
+        users: {
+          [window.current_user.id]: window.current_user
+        },
+        businesses:{}
+      },
+      session: {
+        currentUserId: window.current_user.id
+      },
+    };
+  } else {
+    preloadedState = {
+      entities: {
+        businesses: {}
+      }
+    }
+  }
+
+  const store = configureStore( preloadedState );
 
   const root = document.getElementById('root');
 
