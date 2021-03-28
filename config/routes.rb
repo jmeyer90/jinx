5a48bd9b1891784db5108ffdc928d71daac3eec6
Justin Meyer <meyerj.justin@yahoo.com>
2021-03-28 16:54:21 -0400

    upgrage to ruby 2.6.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  root to: 'static_pages#root'
-
-  namespace :api, default: { format: :json } do
-
-    resources :users, only: [ :create, :show ]
-
-    resource :session, only: [ :create, :destroy ]
-
-    resources :businesses, only: [ :index, :show ] do
-
-      resources :reviews, only: [ :index, :show, :create, :update]
-
-    end
-
-    resources :reviews, only: :destroy 
-
-  end
 end
