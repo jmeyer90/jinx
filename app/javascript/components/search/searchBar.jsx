77bd65ede0d1bd9c4faf7571b91a0a69cd567c3f
Justin Meyer <meyerj.justin@gmail.com>
2021-06-03 16:31:49 -0400

    create search form

--- app/javascript/components/search/searchBar.jsx
+++ app/javascript/components/search/searchBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import usePath from '../../utils/path_util'
 
 const SearchBar = () => {
+  const path = usePath()
+
+  const splashClassName = path == "/" ? "splash-" : ""
+
   return (
-    <p>Search Bar</p>
+    <form action="" className={`${splashClassName}search-bar`}>
+      <label className={`${splashClassName}search-label`}>Find</label>
+
+      <input type="text" 
+        className={`${splashClassName}search-input`}
+        placeholder="wand shops, broomstick repair, potions supply"/>
+
+      <button className={`${splashClassName}search-button`}>🔍</button>
+    </form>
   )
 }
 
