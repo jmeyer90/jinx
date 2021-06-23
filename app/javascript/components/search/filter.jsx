2dde92db87a5240029a77dd6707c8668653083a2
Justin Meyer <meyerj.justin@gmail.com>
2021-06-23 11:45:38 -0400

    add values to filter

--- app/javascript/components/search/filter.jsx
+++ app/javascript/components/search/filter.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Filter = ({category}) => {
+const Filter = ({categories}) => {
   return (
-    <span>{`${category}`} Filter</span>
+    <section>
+      <ul>
+        {categories.map((category, idx) => (
+          <li key={idx} >{category}</li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
--- app/javascript/components/splash/splash.jsx
+++ app/javascript/components/splash/splash.jsx
@@ -10,9 +10,11 @@ const Splash = () => {
       <figure className="splash-img">
         <img className="splash-img-src" src='assets/potions.jpg' alt="background-image" />
       </figure>
-      <Filter category={"Attribute"}/>
+      <h2>Find the Best Businesses in Town</h2>
+      <Filter categories={["Wand Shops", "Broomstick Repair", "Restaurants", "Curse Removal"]}/>
       <section className="splash-details">
-        <Filter category={"Location"}/>
+        <h2>Jinx New York</h2>
+        <Filter categories={["New York", "San Francisco", "Chicago", "Austin", "London"]}/>
         <Businesses />
       </section>
     </section>
