6fac58e35e1b0287f7ea69dd606b02f774f928a3
Justin Meyer <meyerj.justin@gmail.com>
2021-06-30 10:07:14 -0400

    edit splash business index html for styling

--- app/javascript/components/businesses/businessIndexItem.jsx
+++ app/javascript/components/businesses/businessIndexItem.jsx
@@ -7,6 +7,12 @@ const BusinessIndexItem = ({business}) => {
   const review = useSelector(state => state.entities.reviews[business.main_review_id]) 
   const user = useSelector(state => state.entities.users[business.main_reviewer_id])  
 
+  const attributes = [
+    "attr1",
+    "attr2",
+    "attr3"
+  ]
+
   const displayUserReview = () => (
     user && review ? 
       <section className="business-index-review-container">
@@ -16,13 +22,25 @@ const BusinessIndexItem = ({business}) => {
       : null
   )
 
+  const displayAttributes = () => (
+    <ul className="business-index-attribute-container">
+      {attributes.map(attr => (
+        <p key={`${attr}`} className="business-index-attribute">{attr}</p>
+      ))}
+    </ul>
+  )
+
   return (
     <Link className="business-index-container" to={`/businesses/${business.id}`}>
+      <img className="business-index-image" src="" alt="business review image" />
       <h2 className="business-index-title">{business.name}</h2>
-      <span className="business-index-description">
-        <ul className="business-index-rating">{displayRating(business.average_rating)}</ul>
-        {displayUserReview()}
-      </span>
+      <secrtion className="business-index-description">
+        <span>
+          <ul className="business-index-rating">{displayRating(business.average_rating)}</ul>
+          <p className="business-index-num-revuiews">10 reviews</p>
+        </span>
+        {displayAttributes()}
+      </secrtion>
     </Link>
     
   )
