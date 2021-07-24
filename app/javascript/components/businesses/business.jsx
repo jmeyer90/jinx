006417c010a6dac5915416f3d2f4ce4ebeaae782
Justin Meyer <meyerj.justin@gmail.com>
2021-07-24 19:17:05 -0400

    style business map

--- app/javascript/components/businesses/business.jsx
+++ app/javascript/components/businesses/business.jsx
@@ -33,7 +33,9 @@ const Business = () => {
         <section className="loaction-hours">
           <h2 className="business-section-header">Location & Hours</h2>
           <section className="business-map-hours-container">
-            <Map business={business}/>
+            <div className="business-map-container">
+              <Map business={business}/>
+            </div>
             <BusinessHours business={business}/>
           </section>
         </section>
