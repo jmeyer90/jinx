84b6fcb0f95f302b8cd42c8e983cb6d6ef745275
Justin Meyer <meyerj.justin@gmail.com>
2021-07-31 23:08:34 -0400

    check for image_urls

--- app/javascript/components/businesses/buinessImages.jsx
+++ app/javascript/components/businesses/buinessImages.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
 const BusinessImages = ({business}) => {
-  return (
-    <ul className="business-img-container">
-      {business.image_urls.map(image_url => 
+
+  const renderImages = () => (
+    business.image_urls ?
+      business.image_urls.map(image_url => 
         <img
           className="business-img-index"
           alt="image from user review"
           src={`${image_url}`}
         />
-      )}
+      )
+      : null
+  )
+
+  return (
+    <ul className="business-img-container">
+      {renderImages()}
     </ul>
   )
 }
