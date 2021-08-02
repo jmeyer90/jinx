17080b5ea9d62dc4a983bf3dcda43b10c394478e
Justin Meyer <meyerj.justin@gmail.com>
2021-08-01 21:12:18 -0400

    add footer content

--- app/javascript/components/footer/footer.jsx
+++ app/javascript/components/footer/footer.jsx
@@ -6,14 +6,12 @@ const Footer = () =>{
       <section className="footer-details">
         <section className="footer-bio">
           <h1 className="footer-header">About Me</h1>
-          <h1 className="footer-section-header">Justin James Meyer</h1>
-          <p className="footer-bio-item">Full Stack Developer</p>          
-          <p className="footer-bio-item">Sr Instructor for App Academy</p>
-          <p className="footer-bio-item">Rails, React</p>
-          <p className="footer-bio-item">MERN</p>
-          <p className="footer-bio-item">Mathematics</p>
-          <p className="footer-bio-item">Poetry</p>          
-          <p className="footer-bio-item">Hiking</p>
+          <p className="footer-bio-item">Justin James Meyer, based out of Brooklyn, NY</p>
+          <p className="footer-bio-item">Full-Stack Developer.</p>
+          <p className="footer-bio-item">Sr Instructor for App Academy, the top rated coding bootcamp</p>
+          <p className="footer-bio-item">Proficiency in: Rails, JavaScript, React, Redux, the MERN stack, CSS, HTML, and TDD.</p>
+          <p className="footer-bio-item">I have a background in mathematics education and proved an original theorem in Number Theory, which was presented at CANT 2018 and MAA Metro NY.</p>
+          <p className="footer-bio-item">In my spare time, I write poetry and frequently explore hiking trails around New York City.</p>
         </section>
 
         <section className="footer-contact-container">
@@ -25,19 +23,15 @@ const Footer = () =>{
           </a>
           <a className="footer-contact-item" href="https://www.linkedin.com/in/justin-james-meyer/">
             <h1 className="footer-section-header">LinkedIn</h1>
-            <p  className="footer-contact-detail">https://www.linkedin.com/in/justin-james-meyer/</p>
+            <p  className="footer-contact-detail">https://www.linkedin.com/in/justin-james-meyer</p>
           </a>
           <a className="footer-contact-item" href="https://github.com/jmeyer90">
             <h1 className="footer-section-header">Github</h1>
             <p className="footer-contact-detail">https://github.com/jmeyer90</p>
           </a>
-          <a className="footer-contact-item" href="https://github.com/jmeyer90">
+          <a className="footer-contact-item" href="https://angel.co/u/justin-j-meyer">
             <h1 className="footer-section-header">AngleList</h1>
-            <p className="footer-contact-detail">https://github.com/jmeyer90</p>
-          </a>
-          <a className="footer-contact-item" href="https://github.com/jmeyer90">
-            <h1 className="footer-section-header">Hired</h1>
-            <p className="footer-contact-detail">https://github.com/jmeyer90</p>
+            <p className="footer-contact-detail">https://angel.co/u/justin-j-meyer</p>
           </a>
         </section>
       </section>
